9f1ee61dcc334dd0f65d5698b5239fb1ed2b7fed
evxeen <evxeen@gmail.com>
2024-09-12 15:53:37 +0300

    feat(header): added Header component with logo, search bar, and icons
    Создан компонент Header, который включает следующие элементы:\n- Логотип с изображением.\n- Строка поиска с плейсхолдером 'Поиск продукции...'.\n- Иконки для счётчика и пользователя.\n\nИспользуются классы для стилизации: header, logo, header-content, header-input, header-counter и header-user. Подключены стили из файла Header.module.scss.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,113 +1,20 @@
-import logo from '../../assets/images/logo.png';
-import styles from './Header.module.scss';
+import logo from "../../assets/images/logo.png";
+import counter from "../../assets/icons/counter.svg";
+import user from "../../assets/icons/user.svg";
+
+import styles from "./Header.module.scss";
 
 function Header() {
   return (
     <div className={styles.header}>
-      <div>
-        <img className={styles.logo} src={logo} alt="logotype" />
+      <div className="logo">
+        <img src={logo} className="logo-image" alt="logo" />
       </div>
-      <nav className={styles.menu}>
-        <nav>
-          <ul>
-            <li>
-              Отгрузка продуции
-              <svg
-                className={styles.arrow}
-                width="25px"
-                height="25px"
-                fill="none"
-                viewBox="0 0 25 25"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 10L12 15L17 10"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </li>
-            <li>
-              Претензионная работа
-              <svg
-                className={styles.arrow}
-                width="25px"
-                height="25px"
-                fill="none"
-                viewBox="0 0 25 25"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 10L12 15L17 10"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </li>
-            <li>
-              Заказ в производсвто
-              <svg
-                className={styles.arrow}
-                width="25px"
-                height="25px"
-                fill="none"
-                viewBox="0 0 25 25"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 10L12 15L17 10"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </li>
-            <li>
-              Изделеия по чертежам
-              <svg
-                className={styles.arrow}
-                width="25px"
-                height="25px"
-                fill="none"
-                viewBox="0 0 25 25"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 10L12 15L17 10"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </li>
-            <li>
-              Инструменты
-              <svg
-                className={styles.arrow}
-                width="25px"
-                height="25px"
-                fill="none"
-                viewBox="0 0 25 25"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 10L12 15L17 10"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </li>
-          </ul>
-        </nav>
-      </nav>
+      <div className="header-conten">
+        <input className="header-input" placeholder="Поиск продукции..." />
+        <img className="header-counter" src={counter} alt="counter" />
+        <img className="header-user" src={user} alt="counter" />
+      </div>
     </div>
   );
 }
