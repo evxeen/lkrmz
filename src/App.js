5e85fb962f1dc7750d276e98c8749bd55d3acb24
evxeen <evxeen@gmail.com>
2024-09-16 14:33:48 +0300

    refactor: change color icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+
 import Header from "./components/Header/Header";
 import Chapter from "./components/Chapter/Chapter";
 
 import { chapterList } from "./components/Icons";
 
 function App() {
-  console.log(chapterList);
   return (
     <div className="App">
       <div className="container">
