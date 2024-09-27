092def7c86fbdb058db42181b09390625608a0bb
evxeen <evxeen@gmail.com>
2024-09-27 10:24:51 +0300

    fix: template register page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-
 import "../../App.css";
 import EyeOpenIcon from "../../assets/icons/eye.svg";
 import EyeClosedIcon from "../../assets/icons/eye_close.svg";
 
 import styles from "./AuthPage.module.scss";
+import { useState } from "react";
 
 function AuthPage() {
   const [eyesIsOpen, setIsEyeOpen] = useState(false);
@@ -14,29 +13,35 @@ function AuthPage() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.authBlock}>
-        <h2>Авторизация</h2>
-        <input type="text" placeholder="Логин" />
+    <div className={styles.pageWrapper}>
+      <div className={styles.backgroundBlur} />
+      <div className={styles.container}>
+        <div className={styles.authBlock}>
+          <h2>Авторизация</h2>
+          <input type="text" placeholder="Логин" />
 
-        <div className={styles.passwordContainer}>
-          <input type={eyesIsOpen ? "text" : "password"} placeholder="Пароль" />
-          <button
-            onClick={toggleEye}
-            type="button"
-            className={styles.eyeButton}
-          >
-            <img
-              src={eyesIsOpen ? EyeOpenIcon : EyeClosedIcon}
-              alt="toggle visibility"
-              className={styles.eyeIcon}
+          <div className={styles.passwordContainer}>
+            <input
+              type={eyesIsOpen ? "text" : "password"}
+              placeholder="Пароль"
             />
-          </button>
-        </div>
+            <button
+              onClick={toggleEye}
+              type="button"
+              className={styles.eyeButton}
+            >
+              <img
+                src={eyesIsOpen ? EyeOpenIcon : EyeClosedIcon}
+                alt="toggle visibility"
+                className={styles.eyeIcon}
+              />
+            </button>
+          </div>
 
-        <div className={styles.bottomBlock}>
-          <a href="/">Забыли пароль?</a>
-          <button>Войти</button>
+          <div className={styles.bottomBlock}>
+            <a href="/">Забыли пароль?</a>
+            <button>Войти</button>
+          </div>
         </div>
       </div>
     </div>
